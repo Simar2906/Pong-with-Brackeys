c7faeabf2742bf514e5d2ef1b5001602b789ba26
Simar2906 <simar2906@gmail.com>
2020-08-12 08:48:54 +0530

    ball bounce done

--- Pong with Brackeys/Assets/Scripts/ball.cs
+++ Pong with Brackeys/Assets/Scripts/ball.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D myRigidbody = null;
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        float randomNumber = Random.Range(0, 2);
+        if(randomNumber <= 0.5)
+        {
+            Debug.Log("Shoot Right");
+            myRigidbody.AddForce(new Vector2(80, 10));
+        }
+        else
+        {
+            Debug.Log("Shoot Left");
+            myRigidbody.AddForce(new Vector2(-80, -10));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D colInfo) {
+        if(colInfo.collider.tag == "Player")
+        {
+            Vector3 v = GetComponent<Rigidbody2D>().velocity;
+            v.y = myRigidbody.velocity.y/2 + colInfo.collider.attachedRigidbody.velocity.y/3; // addes spin due to paddle
+            GetComponent<Rigidbody2D>().velocity = v;
+        }
     }
 }
