358edd5a68e54c671724700bede6d8f1211cf574
Simar2906 <simar2906@gmail.com>
2020-08-12 09:46:18 +0530

    scoring basics done(with minor error)

---
+++ Pong with Brackeys/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    static int playerScore01 = 0;
+    static int playerScore02 = 0;
+
+    public GUISkin theSkin = null;
+    static public void Score(string wallName)
+    {
+        if(wallName == "rightWall")
+        {
+            playerScore01 += 1;
+        }
+        else if(wallName == "left Wall")
+        {
+            playerScore02 += 1;
+        }
+        Debug.Log("Player score 1 is :" + playerScore01);
+        Debug.Log("Player score 2 is :" + playerScore02);
+
+    }
+
+    private void OnGUI() 
+    {
+        GUI.skin = theSkin;
+        GUI.Label(new Rect(Screen.width/2 - 150, 25, 100, 100), "" + playerScore01);
+        GUI.Label(new Rect(Screen.width/2 + 150, 25, 100, 100), "" + playerScore02);
+    }
+}
---
+++ Pong with Brackeys/Assets/Scripts/SideWalls.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWalls : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D hitInfo) 
+    {
+        if(hitInfo.name == "Ball")
+        {
+            Game_Manager.Score(transform.name);
+        }
+    }
+}
