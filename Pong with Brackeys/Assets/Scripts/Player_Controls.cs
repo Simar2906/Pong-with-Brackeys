38df3beffb6ceb1b7cbc0184d9f8b7c24cef385c
Simar2906 <simar2906@gmail.com>
2020-08-12 08:16:07 +0530

    walls + ball start

--- Pong with Brackeys/Assets/Scripts/Player_Controls.cs
+++ Pong with Brackeys/Assets/Scripts/Player_Controls.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class Player_Controls : MonoBehaviour
 {
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    private Rigidbody2D rigbody = null; 
     public float speed = 10f;
-    void Start()
+
+    private void Awake() 
     {
-        
+        rigbody = GetComponent<Rigidbody2D>();
     }
+    void Update()
+    {
+        if(Input.GetKey(moveUp))
+        {
+            rigbody.velocity = new Vector2(0, speed);
+        }
+        else if(Input.GetKey(moveDown))
+        {
+            rigbody.velocity = new Vector2(0, speed * -1);
+        }
+        else
+        {
+            rigbody.velocity = new Vector2(0, 0);
+            
+        }
+    }
+
 }
